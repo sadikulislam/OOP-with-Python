cb8474813527b2956bc3008424b101f79826f6a6
Sadikul Islam <sadikulislam086@gmail.com>
2024-03-24 00:20:16 +0600

    added subclass

--- Employee.py
+++ Employee.py
@@ -22,12 +22,48 @@ class Employee:
             print("Your email is invalid")
 
     def apply_raise(self):
-        return int(self.salary * self.raise_amt)
+        self.salary = int(self.salary * self.raise_amt)
 
     def display_info(self):
         print(f"Name: {self.fullname()}\nID: {self.id}\nSalary: {self.apply_raise()}\nEmail: {self.email}")
 
 
-emp1 = Employee('John', 'Smith', 100, 50000)
-print(emp1.is_valid_email())
-print(emp1.display_info())
+class Developer(Employee):
+    raise_amt = 1.10
+
+    def __init__(self, first, last, id, salary, prog_lang):
+        super().__init__(first, last, id, salary)
+        self.prog_lang = prog_lang
+
+
+class Manager(Employee):
+    def __init__(self, first, last, id, salary, employees=None):
+        super().__init__(first, last, id, salary)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_employee(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_employee(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_employees(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+
+dev_1 = Developer('Jack', 'Nash', 100, 30000, 'Python')
+dev_2 = Developer('John', 'Smith', 102, 50000, 'Java')
+
+mgr_1 = Manager('Max', 'Joe', 303, 90000,[dev_1])
+
+print(mgr_1.email)
+mgr_1.add_employee(dev_2)
+mgr_1.remove_employee(dev_1)
+mgr_1.print_employees()
+
